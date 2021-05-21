0d99a78f8c3b83fb0a4034ea7c57bd38998ebea9
vundelayamini <vundelayamini@gmail.com>
2021-05-21 14:52:39 +0530

    [yamini]Add: Welcome message to Product review managemet linq demo

---
+++ LINQDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To product review Management LINQ ");
+        }
+    }
+}
