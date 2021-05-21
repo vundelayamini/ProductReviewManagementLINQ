bd9944a7613b699d8362927667937fd1a4b63667
vundelayamini <vundelayamini@gmail.com>
2021-05-21 21:34:02 +0530

    [yamini]Add: Adding missing details of product Review class

---
+++ LINQDemo/ProductReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQDemo
+{
+    class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
